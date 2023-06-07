48cb0325ccc4f1df03be2ba26fd819ce52fb154c
Remy Zulauff <remy.zulauff@gmail.com>
2023-06-07 19:33:39 +0200

    init(afa-consulting): init base Vue app structure with firebase

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import VTransitionFade from './components/VTransitionFade.vue'
+import TheFooter from './layouts/TheFooter.vue'
+import TheHeader from './layouts/TheHeader.vue'
+</script>
+
+<template>
+  <TheHeader />
+  <router-view v-slot="{ Component }">
+    <VTransitionFade>
+      <component :is="Component" />
+    </VTransitionFade>
+  </router-view>
+  <TheFooter />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,297 @@
+<template>
+  <div class="bg-white">
+    <div class="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
+      <div
+        class="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+        aria-hidden="true"
+      />
+      <div class="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
+        <div
+          class="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8"
+        >
+          <h1
+            class="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto"
+          >
+            We’re changing the way people connect.
+          </h1>
+          <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+            <p class="text-lg leading-8 text-gray-600">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
+              commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua. Anim aute id magna
+              aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+            </p>
+            <div class="mt-10 flex items-center gap-x-6">
+              <a
+                href="#"
+                class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >Get started</a
+              >
+              <a href="#" class="text-sm font-semibold leading-6 text-gray-900"
+                >Learn more <span aria-hidden="true">→</span></a
+              >
+            </div>
+          </div>
+          <img
+            src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
+            alt=""
+            class="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+          />
+        </div>
+      </div>
+      <div class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
+    </div>
+
+    <div class="bg-white py-24 sm:py-32">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div
+          class="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5"
+        >
+          <img
+            class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+            alt="Transistor"
+            width="158"
+            height="48"
+          />
+          <img
+            class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+            alt="Reform"
+            width="158"
+            height="48"
+          />
+          <img
+            class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+            alt="Tuple"
+            width="158"
+            height="48"
+          />
+          <img
+            class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+            alt="SavvyCal"
+            width="158"
+            height="48"
+          />
+          <img
+            class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+            alt="Statamic"
+            width="158"
+            height="48"
+          />
+        </div>
+        <div class="mt-16 flex justify-center">
+          <p
+            class="relative rounded-full bg-gray-50 px-4 py-1.5 text-sm leading-6 text-gray-600 ring-1 ring-inset ring-gray-900/5"
+          >
+            <span class="hidden md:inline"
+              >Over 2500 companies use our tools to better their business.</span
+            >
+            <a href="#" class="font-semibold text-indigo-600"
+              ><span class="absolute inset-0" aria-hidden="true" /> Read our customer stories
+              <span aria-hidden="true">&rarr;</span></a
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="bg-white py-24 sm:py-32">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:text-center">
+          <h2 class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Everything you need to deploy your app
+          </p>
+          <p class="mt-6 text-lg leading-8 text-gray-600">
+            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget
+            egestas a elementum pulvinar et feugiat blandit at. In mi viverra elit nunc.
+          </p>
+        </div>
+        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl
+            class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16"
+          >
+            <div v-for="feature in features" :key="feature.name" class="relative pl-16">
+              <dt class="text-base font-semibold leading-7 text-gray-900">
+                <div
+                  class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600"
+                >
+                  <component :is="feature.icon" class="h-6 w-6 text-white" aria-hidden="true" />
+                </div>
+                {{ feature.name }}
+              </dt>
+              <dd class="mt-2 text-base leading-7 text-gray-600">{{ feature.description }}</dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+    <section class="isolate overflow-hidden bg-white px-6 lg:px-8">
+      <div class="relative mx-auto max-w-2xl py-24 sm:py-32 lg:max-w-4xl">
+        <div
+          class="absolute left-1/2 top-0 -z-10 h-[50rem] w-[90rem] -translate-x-1/2 bg-[radial-gradient(50%_100%_at_top,theme(colors.indigo.100),white)] opacity-20 lg:left-36"
+        />
+        <div
+          class="absolute inset-y-0 right-1/2 -z-10 mr-12 w-[150vw] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-20 md:mr-0 lg:right-full lg:-mr-36 lg:origin-center"
+        />
+        <figure class="grid grid-cols-1 items-center gap-x-6 gap-y-8 lg:gap-x-10">
+          <div class="relative col-span-2 lg:col-start-1 lg:row-start-2">
+            <svg
+              viewBox="0 0 162 128"
+              fill="none"
+              aria-hidden="true"
+              class="absolute -top-12 left-0 -z-10 h-32 stroke-gray-900/10"
+            >
+              <path
+                id="b56e9dab-6ccb-4d32-ad02-6b4bb5d9bbeb"
+                d="M65.5697 118.507L65.8918 118.89C68.9503 116.314 71.367 113.253 73.1386 109.71C74.9162 106.155 75.8027 102.28 75.8027 98.0919C75.8027 94.237 75.16 90.6155 73.8708 87.2314C72.5851 83.8565 70.8137 80.9533 68.553 78.5292C66.4529 76.1079 63.9476 74.2482 61.0407 72.9536C58.2795 71.4949 55.276 70.767 52.0386 70.767C48.9935 70.767 46.4686 71.1668 44.4872 71.9924L44.4799 71.9955L44.4726 71.9988C42.7101 72.7999 41.1035 73.6831 39.6544 74.6492C38.2407 75.5916 36.8279 76.455 35.4159 77.2394L35.4047 77.2457L35.3938 77.2525C34.2318 77.9787 32.6713 78.3634 30.6736 78.3634C29.0405 78.3634 27.5131 77.2868 26.1274 74.8257C24.7483 72.2185 24.0519 69.2166 24.0519 65.8071C24.0519 60.0311 25.3782 54.4081 28.0373 48.9335C30.703 43.4454 34.3114 38.345 38.8667 33.6325C43.5812 28.761 49.0045 24.5159 55.1389 20.8979C60.1667 18.0071 65.4966 15.6179 71.1291 13.7305C73.8626 12.8145 75.8027 10.2968 75.8027 7.38572C75.8027 3.6497 72.6341 0.62247 68.8814 1.1527C61.1635 2.2432 53.7398 4.41426 46.6119 7.66522C37.5369 11.6459 29.5729 17.0612 22.7236 23.9105C16.0322 30.6019 10.618 38.4859 6.47981 47.558L6.47976 47.558L6.47682 47.5647C2.4901 56.6544 0.5 66.6148 0.5 77.4391C0.5 84.2996 1.61702 90.7679 3.85425 96.8404L3.8558 96.8445C6.08991 102.749 9.12394 108.02 12.959 112.654L12.959 112.654L12.9646 112.661C16.8027 117.138 21.2829 120.739 26.4034 123.459L26.4033 123.459L26.4144 123.465C31.5505 126.033 37.0873 127.316 43.0178 127.316C47.5035 127.316 51.6783 126.595 55.5376 125.148L55.5376 125.148L55.5477 125.144C59.5516 123.542 63.0052 121.456 65.9019 118.881L65.5697 118.507Z"
+              />
+              <use href="#b56e9dab-6ccb-4d32-ad02-6b4bb5d9bbeb" x="86" />
+            </svg>
+            <blockquote
+              class="text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9"
+            >
+              <p>
+                Commodo amet fugiat excepteur sunt qui ea elit cupidatat ullamco consectetur ipsum
+                elit consequat. Elit sunt proident ea nulla ad nulla dolore ad pariatur tempor non.
+                Sint veniam minim et ea.
+              </p>
+            </blockquote>
+          </div>
+          <div class="col-end-1 w-16 lg:row-span-4 lg:w-72">
+            <img
+              class="rounded-xl bg-indigo-50 lg:rounded-3xl"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=576&h=576&q=80"
+              alt=""
+            />
+          </div>
+          <figcaption class="text-base lg:col-start-1 lg:row-start-3">
+            <div class="font-semibold text-gray-900">Judith Black</div>
+            <div class="mt-1 text-gray-500">CEO of Workcation</div>
+          </figcaption>
+        </figure>
+      </div>
+    </section>
+    <div class="bg-indigo-700">
+      <div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div class="mx-auto max-w-2xl text-center">
+          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Boost your productivity.<br />Start using our app today.
+          </h2>
+          <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-indigo-200">
+            Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua
+            proident excepteur commodo do ea.
+          </p>
+          <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >Get started</a
+            >
+            <a href="#" class="text-sm font-semibold leading-6 text-white"
+              >Learn more <span aria-hidden="true">→</span></a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-white">
+      <div class="mx-auto max-w-7xl px-6 py-24 sm:pt-32 lg:px-8 lg:py-40">
+        <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+          <div class="lg:col-span-5">
+            <h2 class="text-2xl font-bold leading-10 tracking-tight text-gray-900">
+              Frequently asked questions
+            </h2>
+            <p class="mt-4 text-base leading-7 text-gray-600">
+              Can’t find the answer you’re looking for? Reach out to our
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500"
+                >customer support</a
+              >
+              team.
+            </p>
+          </div>
+          <div class="mt-10 lg:col-span-7 lg:mt-0">
+            <dl class="space-y-10">
+              <div v-for="faq in faqs" :key="faq.question">
+                <dt class="text-base font-semibold leading-7 text-gray-900">{{ faq.question }}</dt>
+                <dd class="mt-2 text-base leading-7 text-gray-600">{{ faq.answer }}</dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  FingerPrintIcon,
+  LockClosedIcon
+} from '@heroicons/vue/24/outline'
+
+const features = [
+  {
+    name: 'Push to deploy',
+    description:
+      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+    icon: CloudArrowUpIcon
+  },
+  {
+    name: 'SSL certificates',
+    description:
+      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+    icon: LockClosedIcon
+  },
+  {
+    name: 'Simple queues',
+    description:
+      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+    icon: ArrowPathIcon
+  },
+  {
+    name: 'Advanced security',
+    description:
+      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+    icon: FingerPrintIcon
+  }
+]
+
+const faqs = [
+  {
+    question: 'How do you make holy water?',
+    answer:
+      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.'
+  },
+  {
+    question: 'How do you make holy water?',
+    answer:
+      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.'
+  },
+  {
+    question: 'How do you make holy water?',
+    answer:
+      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.'
+  },
+  {
+    question: 'How do you make holy water?',
+    answer:
+      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.'
+  },
+  {
+    question: 'How do you make holy water?',
+    answer:
+      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.'
+  },
+  {
+    question: 'How do you make holy water?',
+    answer:
+      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.'
+  }
+  // More questions...
+]
+</script>
